a8cc7f2f6266b6af48412a4d8e80a01c80b954fb
TANXINKAI <81215661+TANXINKAI@users.noreply.github.com>
2022-09-15 20:40:28 +0800

    Create Main.cpp

---
+++ B+Tree/Main.cpp
@@ -0,0 +1,19 @@
+#include "BTreeModel.h"
+
+int main() {
+  BPTree node;
+  node.insert(5);
+  node.insert(15);
+  node.insert(25);
+  node.insert(35);
+  node.insert(45);
+
+  node.display(node.getRoot());
+  node.search(15);
+  node.remove(15);
+
+  node.display(node.getRoot());
+
+  node.search(15);
+  node.rangequery(5,35);
+}
