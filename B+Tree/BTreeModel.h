b592f0ec38e1af652b3be0438745a0683c3605e2
Austin Teo <austintjy@gmail.com>
2022-09-19 04:27:47 +0800

    Modification for BTree Integration
    Added block and offset to each BTree (node) data insertion

--- B+Tree/BTreeModel.h
+++ B+Tree/BTreeModel.h
@@ -8,18 +8,38 @@
 #include <string>
 #include <vector>
 using namespace std;
-const int MAX = 3;
 
+int MAX = 3;
 class BPTree;
 class Node
 {
 	bool IS_LEAF;
 	int *key, size;
 	Node **ptr;
+	int *addressBlock;
+	int *addressOffset;
 	friend class BPTree;
 
 public:
 	Node();
+	bool isLeaf() {
+		return IS_LEAF;
+	}
+	int getSize() {
+		return this->size;
+	}
+	int* getKeys() {
+		return this->key;
+	}
+	int* getBlocks() {
+		return this->addressBlock;
+	}
+	int* getOffsets() {
+		return this->addressOffset;
+	}
+	Node** getPointers() {
+		return this->ptr;
+	}
 };
 class BPTree
 {
@@ -28,40 +48,48 @@ class BPTree
 	int height = 0; // height of tree, n = Max = 3
 	int no_nodes; // number of nodes in the tree
 
-	void insertInternal(int, Node *, Node *);
+	void insertInternal(int, int, int, Node *, Node *);
 	void removeInternal(int, Node *, Node *);
 	Node *findParent(Node *, Node *);
 
 public:
-	BPTree();
+
+	BPTree(int n);
 	void search(int);
 	void rangequery(int, int);
-	void insert(int);
+	void insert(int, int, int);
 	void remove(int);
 	void display(Node *);
 	void display(Node *, int, vector<string>*);
 	Node *getRoot();
 	int count_nodes(Node *);
+	int getTreeOrder();
+	int getHeight();
 };
 Node::Node()
 {
 	key = new int[MAX];   // creating an array of length N/Max (Each node should have N keys)
-	ptr = new Node *[MAX+1];   // Whats the purpose of this? i thought the keys within the same node is in an array... if use ptr, shouldnt key = new Node[MAX] or smth?
+	addressBlock = new int[MAX];
+	addressOffset = new int[MAX];
+	ptr = new Node *[MAX + 1];   // Whats the purpose of this? i thought the keys within the same node is in an array... if use ptr, shouldnt key = new Node[MAX] or smth?
 	//MAX + 1 because MAX = number of keys. Pointer count = number of keys + 1. Hence MAX + 1
 	for (int i = 0; i < MAX + 1; i++) //Initialize all ptrs to nullptr. Uninitialized pointers are problematic when trying to check for validity.
 		ptr[i] = nullptr;
 }
-BPTree::BPTree()
+BPTree::BPTree(int n)
 {
 	root = nullptr;
+	MAX = n;
 }
-void BPTree::insert(int x)
+void BPTree::insert(int x, int block, int offset)
 {
 	this->keyCount++;
 	if (root == nullptr)
 	{
 		root = new Node;
 		root->key[0] = x;
+		root->addressBlock[0] = block;
+		root->addressOffset[0] = offset;
 		root->IS_LEAF = true;
 		root->size = 1; // size of root node = 1
 		this->height = 1;
@@ -75,7 +103,7 @@ void BPTree::insert(int x)
 			parent = cursor;
 			for (int i = 0; i < cursor->size; i++)
 			{
-				if (x < cursor->key[i])
+				if (x <= cursor->key[i])
 				{
 					cursor = cursor->ptr[i];
 					break;
@@ -94,8 +122,14 @@ void BPTree::insert(int x)
 				i++;
 
 			for (int j = cursor->size; j > i; j--) //Shift all keys larger than x to the right by 1
+			{
 				cursor->key[j] = cursor->key[j - 1];
+				cursor->addressBlock[j] = cursor->addressBlock[j - 1];
+				cursor->addressOffset[j] = cursor->addressOffset[j - 1];
+			}
 			cursor->key[i] = x;
+			cursor->addressBlock[i] = block;
+			cursor->addressOffset[i] = offset;
 			cursor->size++;   // Increase size of Node after each "insertion" to the key array
 
 
@@ -106,10 +140,14 @@ void BPTree::insert(int x)
 		else // If current cursor node size == N/Max Keys in Node
 		{
 			Node *newLeaf = new Node;
-			int virtualNode[MAX + 1];
+			int* virtualNode = new int[MAX + 1];
+			int* virtualNodeBlock = new int[MAX + 1];
+			int* virtualNodeOffset = new int[MAX + 1];
 			for (int i = 0; i < MAX; i++)
 			{
 				virtualNode[i] = cursor->key[i];
+				virtualNodeBlock[i] = cursor->addressBlock[i];
+				virtualNodeOffset[i] = cursor->addressOffset[i];
 			}
 			int i = 0, j;
 			while (x > virtualNode[i] && i < MAX)
@@ -117,31 +155,44 @@ void BPTree::insert(int x)
 			for (int j = MAX + 1; j > i; j--)
 			{
 				virtualNode[j] = virtualNode[j - 1];
+				virtualNodeBlock[j] = cursor->addressBlock[j - 1];
+				virtualNodeOffset[j] = cursor->addressOffset[j - 1];
 			}
 			virtualNode[i] = x;
+			virtualNodeBlock[i] = block;
+			virtualNodeOffset[i] = offset;
 			newLeaf->IS_LEAF = true;
 			newLeaf->size = MAX + 1 - (MAX + 1) / 2;
 
 			// Point the new leaf's last pointer to the right siblings last pointer
-			if(cursor->ptr[cursor->size] != nullptr && cursor->ptr[cursor->size]->ptr[cursor->ptr[MAX]->size] != nullptr)
+			if (cursor->ptr[cursor->size] != nullptr && cursor->ptr[cursor->size]->ptr[cursor->ptr[MAX]->size] != nullptr)
 				newLeaf->ptr[newLeaf->size] = cursor->ptr[cursor->size]->ptr[cursor->ptr[MAX]->size];
 
 			cursor->size = (MAX + 1) / 2;
 			cursor->ptr[cursor->size] = newLeaf;
-			
+
 			cursor->ptr[MAX] = nullptr;
 			for (i = 0; i < MAX + 1; i++)
 			{
-				if(i < cursor->size)
+				if (i < cursor->size) {
 					cursor->key[i] = virtualNode[i];
-				else
+					cursor->addressBlock[i] = virtualNodeBlock[i];
+					cursor->addressOffset[i] = virtualNodeOffset[i];
+
+				}
+				else {
 					newLeaf->key[i - cursor->size] = virtualNode[i];
+					newLeaf->addressBlock[i - cursor->size] = virtualNodeBlock[i];
+					newLeaf->addressOffset[i - cursor->size] = virtualNodeOffset[i];
+				}
 			}
 
 			if (cursor == root)
 			{
 				Node *newRoot = new Node;
 				newRoot->key[0] = newLeaf->key[0];
+				newRoot->addressBlock[0] = newLeaf->addressBlock[0];
+				newRoot->addressOffset[0] = newLeaf->addressOffset[0];
 				newRoot->ptr[0] = cursor;
 				newRoot->ptr[1] = newLeaf;
 				newRoot->IS_LEAF = false;
@@ -151,7 +202,7 @@ void BPTree::insert(int x)
 			}
 			else
 			{
-				insertInternal(newLeaf->key[0], parent, newLeaf); // create a new internal node in B+ tree
+				insertInternal(newLeaf->key[0], newLeaf->addressBlock[0], newLeaf->addressOffset[0], parent, newLeaf); // create a new internal node in B+ tree
 			}
 		}
 	}
@@ -240,7 +291,7 @@ void BPTree::rangequery(int lb, int hb)
 		cout << " Not found\n";
 	}
 }
-void BPTree::insertInternal(int x, Node *cursor, Node *child) // insert a new internal node in B+ tree
+void BPTree::insertInternal(int x, int block, int offset, Node *cursor, Node *child) // insert a new internal node in B+ tree
 {
 	if (cursor->size < MAX) // Mainly to update the Keys in parent's node
 	{
@@ -249,25 +300,35 @@ void BPTree::insertInternal(int x, Node *cursor, Node *child) // insert a new in
 			i++;
 		for (int j = cursor->size + 1; j > i; j--)
 		{
-			if(j > i && j <= cursor->size)
+			if (j > i && j <= cursor->size) {
 				cursor->key[j] = cursor->key[j - 1];
-			cursor->ptr[j] = cursor->ptr[j-1];
+				cursor->addressBlock[j] = cursor->addressBlock[j - 1];
+				cursor->addressOffset[j] = cursor->addressOffset[j - 1];
+			}
+			cursor->ptr[j] = cursor->ptr[j - 1];
 		}
 		cursor->key[i] = x;
+		cursor->addressBlock[i] = block;
+		cursor->addressOffset[i] = offset;
 		cursor->size++;
 		cursor->ptr[i + 1] = child;
 	}
 	else
 	{
 		Node *newInternal = new Node;
-		int virtualKey[MAX + 1];  // temp key array. Will have N + 1 keys, need to split
-		Node *virtualPtr[MAX + 2];
+		int* virtualKey = new int[MAX + 1];  // temp key array. Will have N + 1 keys, need to split
+		int* virtualBlock = new int[MAX + 1];
+		int* virtualOffset = new int[MAX + 1];
+		Node **virtualPtr = (Node**)malloc(sizeof(Node)*(MAX + 2));
 		for (int i = 0; i < MAX + 1; i++)
 			virtualPtr[i] = nullptr;
 		for (int i = 0; i < MAX + 2; i++) // (For Loop) is for Left sub tree? (Austin: No, this is for cloning the entire cursor key and pointers into virtualKey and virtualPtr respectively)
 		{
-			if(i < MAX)
+			if (i < MAX) {
 				virtualKey[i] = cursor->key[i];
+				virtualBlock[i] = cursor->addressBlock[i];
+				virtualOffset[i] = cursor->addressOffset[i];
+			}
 			virtualPtr[i] = cursor->ptr[i];
 		}
 		int i = 0, j;
@@ -276,12 +337,17 @@ void BPTree::insertInternal(int x, Node *cursor, Node *child) // insert a new in
 
 		for (int j = MAX + 1; j > i; j--) // (For Loop) is for Right sub tree? (Austin: No, this is for shifting keys and pointers to the right (of index i))
 		{
-			if (i < MAX)
+			if (i < MAX) {
 				virtualKey[j] = virtualKey[j - 1];
+				virtualBlock[j] = virtualBlock[j - 1];
+				virtualOffset[j] = virtualOffset[j - 1];
+			}
 			virtualPtr[j] = virtualPtr[j - 1];
 		}
 
 		virtualKey[i] = x;
+		virtualBlock[i] = x;
+		virtualOffset[i] = x;
 		virtualPtr[i + 1] = child;
 		newInternal->IS_LEAF = false;
 		cursor->size = (MAX + 1) / 2;
@@ -289,14 +355,20 @@ void BPTree::insertInternal(int x, Node *cursor, Node *child) // insert a new in
 
 		for (i = 0, j = cursor->size + 1; i < newInternal->size + 2; i++, j++) // Assigning Left Sub tree? WHat about right sub tree? (Austin: This is for creating internal node, there is no left/right tree. See next 2 comments)
 		{
-			if(i < newInternal->size)
+			if (i < newInternal->size) {
 				newInternal->key[i] = virtualKey[j];
+				newInternal->addressBlock[i] = virtualBlock[j];
+				newInternal->addressOffset[i] = virtualOffset[j];
+			}
 			newInternal->ptr[i] = virtualPtr[j];
 		}
+		free(virtualPtr);
 		if (cursor == root) // Need somewhere to Update the Height and Number of Nodes in B+ tree
 		{
 			Node *newRoot = new Node;
 			newRoot->key[0] = cursor->key[cursor->size];
+			newRoot->addressBlock[0] = cursor->addressBlock[cursor->size];
+			newRoot->addressOffset[0] = cursor->addressOffset[cursor->size];
 			//Austin: Point the internal node pointers to left and right sub tree accordingly
 			newRoot->ptr[0] = cursor;
 			newRoot->ptr[1] = newInternal;
@@ -307,7 +379,7 @@ void BPTree::insertInternal(int x, Node *cursor, Node *child) // insert a new in
 		}
 		else // Update the parent Node's key
 		{
-			insertInternal(cursor->key[cursor->size], findParent(root, cursor), newInternal);
+			insertInternal(cursor->key[cursor->size], cursor->addressBlock[cursor->size], cursor->addressOffset[cursor->size], findParent(root, cursor), newInternal);
 		}
 	}
 }
@@ -636,7 +708,7 @@ void BPTree::display(Node *cursor)
 	//Create a vector (array) of strings. 1 for each height of B+ tree
 	vector<string> lines;
 	for (int i = 0; i < this->height; i++)
-		lines.push_back(""); 
+		lines.push_back("");
 	display(cursor, 0, &lines);
 
 	//Best effort beautify print of each level
@@ -659,7 +731,7 @@ void BPTree::display(Node *cursor, int level, vector<string>* txtOutput)
 	{
 		for (int i = 0; i < cursor->size + 1; i++)
 		{
-			display(cursor->ptr[i],level + 1,txtOutput);
+			display(cursor->ptr[i], level + 1, txtOutput);
 			(*txtOutput)[level] = (*txtOutput)[level].append("    ");
 		}
 	}
@@ -684,4 +756,12 @@ int BPTree::count_nodes(Node *cursor) {
 			retVal += count_nodes(cursor->ptr[i]);
 	}
 	return retVal;
+}
+
+int BPTree::getTreeOrder() {
+	return MAX;
+}
+
+int BPTree::getHeight() {
+	return this->height;
 }
